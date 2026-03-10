5ab875e91564760484721de07e3f21b5a0b174a8
ffffyyyyy <yixin1.25@intl.zju.edu.cn>
2026-03-10 20:07:25 +0800

    Update infection.py

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,1 +1,16 @@
+total_students = 91          # Total students in the class
+initial_infected = 5         # Initial number of infected students (day 0)
+growth_rate = 0.4            # Daily infection growth rate (40%)
+# Initialise variables
+current_infected = initial_infected
+days = 0
 
+# Infection loop
+while current_infected < total_students:
+  days += 1
+  new_infected = initial_infected * growth_rate
+  if current_infected + new_infected > total_students:
+    new_infected = total_students - current_infected
+  current_infected += new_infected
+  print("Day",days," new infected:",new_infected," current_infected:",current_infected)
+print("Total days to infect all: ",days)
