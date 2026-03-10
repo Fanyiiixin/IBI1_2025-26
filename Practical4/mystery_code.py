76fbd0df51c2b15909ea3d162e41833b36547152
ffffyyyyy <yixin1.25@intl.zju.edu.cn>
2026-03-10 21:21:46 +0800

    Update mystery_code.py

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:
+# Answer: Generating random integers from 1 to 10 for 11 times, summing them up, and print the final result.
 
 # Import libraries
 # randint allows drawing a random number,
@@ -9,12 +9,15 @@ from random import randint
 # ceil takes the ceiling of a number, i.e. the next higher integer.
 # e.g. ceil(4.2)=5
 from math import ceil
-
+# Initialise total grand and porgress
 total_rand = 0
 progress=0
+# Loop while progress is less than or equal to 10
 while progress<=10:
+    # Increase progress by 1 at the start of each loop
 	progress+=1
 	n = randint(1,10)
+	# Add the random number n to the total sum
 	total_rand+=n
 
 print(total_rand)
